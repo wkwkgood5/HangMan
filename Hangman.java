3df13f2aeecde43881fa36cdd82edd984048f250
wkwkgood5 <p95.kangwang@gmail.com>
2020-03-16 00:50:00 +0800

    initial submit

--- Hangman.java
+++ Hangman.java
@@ -2,7 +2,7 @@ public class Hangman {
 
   public static void main(String[] args) {
     // Your incredible code goes here...
-    Game game = new Game("evelyn");
+    Game game = new Game("ebay");
     Prompter prompter = new Prompter(game);
 
     while (game.getRemainingTries() > 0 && !game.isWon()) {
